0e7bbc69b44f6f0fe30664ee8c752c2f02e0141d
Руслан <seonik1988@gmail.com>
2012-07-19 02:09:04 +0500

    добавил очистку базы добавил отправку базы на почту

---
+++ Source/Classes/Utils/UIAlertView/UIAlertView+Autorelease.h
@@ -0,0 +1,13 @@
+//
+//  UIAlertView+UIAlertView_Autorelease.h
+//  Expenses
+//
+//  Created by MacBook iAPPLE on 19.07.12.
+//  Copyright (c) 2012 AppMake.Ru. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIAlertView (UIAlertView_Autorelease)
+
+@end
---
+++ Source/Classes/Utils/UIAlertView/UIAlertView+Autorelease.m
@@ -0,0 +1,13 @@
+//
+//  UIAlertView+UIAlertView_Autorelease.m
+//  Expenses
+//
+//  Created by MacBook iAPPLE on 19.07.12.
+//  Copyright (c) 2012 AppMake.Ru. All rights reserved.
+//
+
+#import "UIAlertView+UIAlertView_Autorelease.h"
+
+@implementation UIAlertView (UIAlertView_Autorelease)
+
+@end
