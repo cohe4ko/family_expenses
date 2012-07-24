388a719b22d76073942fd794abacb2132b9350f4
Руслан <seonik1988@gmail.com>
2012-07-24 23:35:53 +0500

    fix

---
+++ Source/Classes/ViewController/Other/iPhone/PasswordViewController_iPhone.h
@@ -0,0 +1,13 @@
+//
+//  PasswordViewController_iPhone.h
+//  Expenses
+//
+//  Created by MacBook iAPPLE on 24.07.12.
+//  Copyright (c) 2012 AppMake.Ru. All rights reserved.
+//
+
+#import "PasswordViewController.h"
+
+@interface PasswordViewController_iPhone : PasswordViewController
+
+@end
